9569af5a967f13557803ee4d2ed45c7dbcd5782f
tuan huy <100185063+huynopro102@users.noreply.github.com>
2024-02-03 14:50:45 +0700

    change database sql6.freesqldatabase.com

--- model/connectdbUser.js
+++ model/connectdbUser.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2/promise")
 
 
 const poolUser = mysql.createPool({
-  host: 'sql12.freesqldatabase.com',
-  user: 'sql12675906',
-  password: 'T5fy6qljea',
-  database: 'sql12675906'
+  host: 'sql6.freesqldatabase.com',
+  user: 'sql6681696',
+  password: 'NNd6V4AiAT',
+  database: 'sql6681696'
   });
   
 
