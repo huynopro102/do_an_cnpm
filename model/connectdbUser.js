aea153cacfeaf9fa803c6208ec71a669d5e44958
tuan huy <100185063+huynopro102@users.noreply.github.com>
2024-02-11 11:01:04 +0700

    Update connectdbUser.js

--- model/connectdbUser.js
+++ model/connectdbUser.js
@@ -3,9 +3,9 @@ const mysql = require("mysql2/promise")
 
 const poolUser = mysql.createPool({
   host: 'sql6.freesqldatabase.com',
-  user: 'sql6681696',
-  password: 'NNd6V4AiAT',
-  database: 'sql6681696'
+  user: 'sql6683327',
+  password: '1xCXAsIWST',
+  database: 'sql6683327'
   });
   
 
