05f5b59d89bffbf1a67501bcfba03d376060e366
tuan huy <100185063+huynopro102@users.noreply.github.com>
2024-03-15 22:59:31 +0700

    Update connectDB.js

--- model/connectDB.js
+++ model/connectDB.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2/promise');
 // create the connection to database
  const pool = mysql.createPool({
      port : '3306',
-   host: '14.225.218.25',
+   host: 'huynguyen-nginx.io.vn',
   user: 'huynguyen',
   password: '12345678',
   database: 'nodejsbasic'
