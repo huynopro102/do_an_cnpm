f45555035528173be45a714ae8f84426078e9116
tuan huy <100185063+huynopro102@users.noreply.github.com>
2024-03-18 00:07:10 +0700

    Update connectdbUser.js

--- model/connectdbUser.js
+++ model/connectdbUser.js
@@ -2,11 +2,11 @@ const mysql = require("mysql2/promise")
 
 
 const poolUser = mysql.createPool({
-   port : '3306',
-   host: 'huynguyen-nginx.io.vn',
-  user: 'huynguyen',
-  password: '12345678',
-  database: 'nodejsbasic'
+ port: "3306",
+  host: "huynguyen-nginx.io.vn",
+  user: "huynguyen1",
+  password: "123",
+  database: "nodejsbasic",
   });
   
 
